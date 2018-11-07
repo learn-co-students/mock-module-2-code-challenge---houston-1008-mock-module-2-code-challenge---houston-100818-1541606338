3adf5b41c98275e2ec87b6a5c080cb5bf65b0180
barbaradecares <barbaradecares@hotmail.com>
2018-11-07 11:22:42 -0600

    done

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,37 @@
 class EmployeesController < ApplicationController
+    before_action :current_employee
+
+    def current_employee
+        if params[:id]
+            @employee = Employee.find(params[:id])
+        else 
+            @employee = Employee.new
+        end
+    end
+
+    def index
+        @employees = Employee.all
+    end 
+
+    def employee_params
+        params.require(:employee).permit(:dog_id,
+            :first_name, 
+            :last_name, 
+            :alias, 
+            :title, 
+            :office, 
+            :img_URL)
+    end 
+
+    def create 
+        @employee = Employee.create(employee_params)
+        
+        redirect_to employee_path(@employee)
+    end
+
+    def update 
+        @employee.update(employee_params)
+        redirect_to employee_path(@employee)
+    end 
+   
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,15 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates_uniqueness_of :alias, if: :different_than_none?
+    validates_uniqueness_of :title
+     
+ 
+    def different_than_none?
+        self.alias != "none"
+    end
+        
+    
+    def name 
+        first_name + " " + last_name
+    end 
 end
