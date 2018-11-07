e511908183560d5ed4c2290cd3bdc9a22e6c60a7
AHCarl <ahcarlin@gmail.com>
2018-11-07 10:53:35 -0600

    migration, seed, models, index & show

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,25 @@
 class EmployeesController < ApplicationController
+  before_action :my_emp 
+
+  def index 
+  end 
+
+  def show 
+  end 
+
+  def create 
+
+  end
+
+  def edit 
+
+  end
+
+  def my_emp 
+    if params[:id]
+      @emp = Employee.find(params[:id])
+    else 
+      @emps = Employee.all
+    end
+  end
 end
