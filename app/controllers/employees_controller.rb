5b2e0dc31b9dcf707aa105ad6d3b5dac067744bb
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-11-07 11:57:25 -0600

    done

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,23 @@
 class EmployeesController < ApplicationController
+    def create
+        Employee.create(employee_params)
+        redirect_to employees_path
+    end
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+     def edit 
+        @dogs = Dog.all
+    end
+
+    def employee_params(*args)
+        params.require(:employee).permit(*args)
+    end
+
 end
