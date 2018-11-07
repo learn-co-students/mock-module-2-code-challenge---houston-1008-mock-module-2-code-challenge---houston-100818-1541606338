98daf949dcefc0233c312de7e2c2c85ed49b4081
github username <github email address>
2018-11-07 10:45:49 -0600

    All Controllers Updated

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,34 @@
 class DogsController < ApplicationController
+    def new
+      @dog = Dog.new
+    end
+    
+    def create
+      @dog = Dog.create(dog_params)
+      redirect_to dog_path
+    end
+    
+    def index
+      @dogs = Dog.all
+    end
+    
+    def show
+      @dog = Dog.find(params[:id])
+    end
+    
+    def edit
+      @dog = Dog.find(params[:id])
+    end
+    
+    def update
+      @dog = Dog.find(params[:id])
+      @dog.update(dog_params)
+      redirect_to @dog
+    end
+
+    private
+    def dog_params
+      params.require(:dog).permit(:name, :breed, :age)
+    end
+    
 end
