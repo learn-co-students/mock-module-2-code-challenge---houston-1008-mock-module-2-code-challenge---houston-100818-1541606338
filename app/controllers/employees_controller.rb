6be10b3ac466cc4cce5b52ac56f741fd4cc62c83
github username <github email address>
2018-11-07 12:12:36 -0600

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -29,7 +29,7 @@ class EmployeesController < ApplicationController
   
     private
     def employee_params
-      params.require(:employee).permit(:first_name, :last_name :alias, :title, :office, :img_url, :dog_id)
+      params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
     end
 
 end
