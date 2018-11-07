6b753d56c851822463cf4ebff21e41a4c6faaba5
AHCarl <ahcarlin@gmail.com>
2018-11-07 11:57:21 -0600

    done for now

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -13,11 +13,22 @@ class EmployeesController < ApplicationController
   end
 
   def create 
-    @emp = Employee.new 
-
+    if Employee.create(emp_params).valid?
+      @emp = Employee.create(emp_params)
+      @emp.save
+    else 
+      puts "Invalid creation"
+    end
+    redirect_to employees_path
   end
 
   def edit 
+    @dogs = Dog.all
+  end
+
+  def update 
+    @emp.update(emp_params)
+    redirect_to employee(@emp)
   end
 
   def my_emp 
@@ -26,5 +37,11 @@ class EmployeesController < ApplicationController
     else 
       @emps = Employee.all
     end
+  end
+
+  private 
+
+  def emp_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url)
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,19 +1,11 @@
 class Employee < ApplicationRecord
   belongs_to :dog
+  validates :title, :alias, absence: true, on: :create
 
   def full_name
     first_name + " " + last_name
   end
 
-  def self.aliases 
-    Employee.all.map do |emp|
-      emp.alias 
-    end
-  end
+  
 
-  def self.titles 
-    Employee.all.map do |emp|
-      emp.title 
-    end
-  end
 end
